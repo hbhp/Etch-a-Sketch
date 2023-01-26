bd7a9c9a7527f8889f39c21cd4216aba02201a7a
Hallam Breen <hal.breen95@gmail.com>
2023-01-26 16:52:24 +0000

    Build functions in Java and add buttons

--- Javascript.js
+++ Javascript.js
@@ -2,16 +2,71 @@ const container = document.querySelector('#container');
 const grid = document.querySelector('#grid');
 
 container.appendChild(grid);
+let gridNum = 16
 
-for(x=0; x<16 ;x++) {
+for(x=0; x< gridNum; x++) {
     const col = document.createElement('div');
     col.classList.add('col');
+    col.id='col'
     grid.appendChild(col);
-    
-    for(n=0; n<16 ;n++) {
+   
+    for(n=0; n<gridNum ;n++) {
     const row = document.createElement('div');
     row.classList.add('row');
     col.appendChild(row);
+    row.id='row'
+    row.addEventListener("mouseenter",blackClick);
     }
 
+}
+
+function add(){
+    if (document.getElementById('gridNum').value > 3 && document.getElementById('gridNum').value < 101){
+    let grid = document.getElementById('grid');
+    grid.innerHTML = '';
+for(x=0; x< document.getElementById('gridNum').value; x++) {
+    const col = document.createElement('div');
+    col.classList.add('col');
+    grid.appendChild(col);
+   
+    for(n=0; n<document.getElementById('gridNum').value ;n++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    col.appendChild(row);
+    row.addEventListener("mouseenter",blackClick);
+    }
+}
+}
+    else {
+    alert('Woops! Something went wrong. Make sure you entered a number between 0-100!')
+                 
+
+    }
+}
+
+function reset() {
+    let grid = document.getElementById('grid');
+    grid.innerHTML = '';
+    for(x=0; x< 16; x++) {
+        const col = document.createElement('div');
+        col.classList.add('col');
+        grid.appendChild(col);
+       
+        for(n=0; n<16 ;n++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        col.appendChild(row);
+        row.addEventListener("mouseenter",blackClick);
+        }
+}
+}
+
+function wipe() {
+    for(x=0; x< document.getElementsByClassName("black").length; x++) { //THIS IS CURRENTLY LOOPING THE CODE HALF THE NUMEBR OF TIMES THE CLASS OCCURS
+    document.getElementsByClassName("black").row.classList.remove('black');   
+}
+}
+
+function blackClick(){
+    this.classList.add('black'); 
 }
